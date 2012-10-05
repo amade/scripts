ea627e0968e46a3217b8fdd6492dd7899fe25861
Amadeusz Sławiński <amade@asmblr.net>
2012-10-06 01:26:37 +0200

    Background script, random + browsing through files

---
+++ x11/bg.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+configfile="${HOME}/.background"
+
+usage () {
+	echo -e "\e[31mUsage: ${0} [files dir] [random|previous|next]\e[0m"
+	exit 1
+}
+
+main () {
+	if [ ${#} -ne 2 ]; then
+		usage
+	fi
+
+	if [ ! -d ${1} ]; then
+		usage
+	fi
+
+	filenum=0
+	if [ -f ${configfile} ]; then
+		read filenum < ${configfile}
+	else
+		filenum=1
+	fi
+
+	n_files=`ls -1 "${1}" | wc -l`
+
+	case $2 in
+		"previous")
+			filenum=$((filenum - 1))
+			if [ ${filenum} -le 0 ]; then
+				filenum=${n_files}
+			fi
+			;;
+		"next")
+			filenum=$(((filenum + 1) % ${n_files}))
+			;;
+		"random")
+			filenum=$(( ${RANDOM} % ${n_files} + 1 ))
+			;;
+		*)
+			usage
+	esac
+
+	echo ${filenum} > ${configfile}
+
+	file=`ls -1 "${1}" | head -${filenum} | tail -1`
+
+	wmsetbg -S -f -u "${1}/${file}"
+}
+
+main ${@}
