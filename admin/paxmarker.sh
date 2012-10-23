9cd837cb905f127a4d909084c76bb1d9a4148c98
Amadeusz Sławiński <amade@asmblr.net>
2012-10-23 19:47:52 +0200

    paxctl -m everything using libGL.so
    differentiate between stuff needed on nouveau and nvidia

--- admin/paxmarker.sh
+++ admin/paxmarker.sh
@@ -1,21 +1,41 @@
-#!/bin/sh
+#!/bin/bash
 
-
-files=(	\
-	/usr/bin/kwin \
-	/usr/bin/mplayer \
-	/usr/bin/mplayer2 \
-	/usr/bin/systemsettings \
-	/usr/bin/vlc \
-	/usr/games/bin/xonotic-glx \
-	/usr/lib64/kde4/libexec/kwin_opengl_test \
-	/usr/lib64/virtualbox/VBoxTestOGL \
-	/usr/libexec/gnome-session-check-accelerated-helper
+case ${1} in
+nouveau)
+	files=(	\
+		/usr/bin/kwin \
+		/usr/bin/mplayer \
+		/usr/bin/mplayer2 \
+		/usr/bin/systemsettings \
+		/usr/bin/vlc \
+		/usr/games/bin/xonotic-glx \
+		/usr/lib64/kde4/libexec/kwin_opengl_test \
+		/usr/lib64/virtualbox/VBoxTestOGL \
+		/usr/libexec/gnome-session-check-accelerated-helper
 	)
 
-for i in $(seq 0 $((${#files[@]} -1))); do
-	if [ -e ${files[${i}]} ]; then
-		echo -e "\033[31mpaxmarking\033[0m ${files[${i}]}"
-		paxctl -m ${files[${i}]}
-	fi
-done
+	for i in $(seq 0 $((${#files[@]} -1))); do
+		if [ -e ${files[${i}]} ]; then
+			paxctl -m ${files[${i}]}
+			echo -e "\033[31mpaxmarked\033[0m ${files[${i}]}"
+		fi
+	done
+	;;
+nvidia)
+	for i in `/bin/ls -1 /usr/bin/`; do
+		ldd "/usr/bin/${i}" 2>&1 >&- | grep "error while loading shared libraries: libGL.so.1" > /dev/null
+		if [ ${?} == 0 ]; then
+			paxctl -m "/usr/bin/${i}"
+			if [ ${?} == 1 ]; then
+				echo -e "\033[31madding header\033[0m /usr/bin/${i}"
+				paxctl -C "/usr/bin/${i}"
+				paxctl -m "/usr/bin/${i}"
+			fi
+			echo -e "\033[31mpaxmarked\033[0m /usr/bin/${i}"
+		fi
+	done
+	;;
+*)
+	echo "Usage: ${0} [nouveau|nvidia]"
+esac
+
