d5d72935f61d7685ec446a2c03188db78c71c539
Amadeusz Sławiński <amade@asmblr.net>
2012-11-07 19:14:23 +0100

    paxmarker.sh - mark files differently for nvidia driver

--- admin/paxmarker.sh
+++ admin/paxmarker.sh
@@ -34,6 +34,31 @@ nvidia)
 			echo -e "\033[31mpaxmarked\033[0m /usr/bin/${i}"
 		fi
 	done
+	for i in `/bin/ls -1 /usr/lib64/`; do
+		ldd "/usr/lib64/${i}" 2>&1 >&- | grep "error while loading shared libraries: libGL.so.1" > /dev/null
+		if [ ${?} == 0 ]; then
+			paxctl -m "/usr/lib64/${i}"
+			if [ ${?} == 1 ]; then
+				echo -e "\033[31madding header\033[0m /usr/lib64/${i}"
+				paxctl -C "/usr/lib64/${i}"
+				paxctl -m "/usr/lib64/${i}"
+			fi
+			echo -e "\033[31mpaxmarked\033[0m /usr/lib64/${i}"
+		fi
+	done
+	for i in `/bin/ls -1 /usr/lib64/kde4/`; do
+		ldd "/usr/lib64/kde4/${i}" 2>&1 >&- | grep "error while loading shared libraries: libGL.so.1" > /dev/null
+		if [ ${?} == 0 ]; then
+			paxctl -m "/usr/lib64/kde4/${i}"
+			if [ ${?} == 1 ]; then
+				echo -e "\033[31madding header\033[0m /usr/lib64/kde4/${i}"
+				paxctl -C "/usr/lib64/kde4/${i}"
+				paxctl -m "/usr/lib64/kde4/${i}"
+			fi
+			echo -e "\033[31mpaxmarked\033[0m /usr/lib64/kde4/${i}"
+		fi
+	done
+
 	;;
 *)
 	echo "Usage: ${0} [nouveau|nvidia]"
