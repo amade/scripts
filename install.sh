cf48774cf22f28e488d7ae1bbe598d8296ec4d4f
Amadeusz Sławiński <amade@asmblr.net>
2013-08-25 22:58:43 +0200

    install stuff globally in /usr/local/(s)bin

--- install.sh
+++ install.sh
@@ -2,24 +2,21 @@
 
 DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-echo $DIR
-
-if [[ ! -d ~/bin ]]; then
-	mkdir ~/bin
+if [[ `whoami` !=  "root" ]]; then
+	echo "need root access"
+	exit 1
 fi
 
-if [[ `whoami` ==  "root" ]]; then
-	for i in `ls ${DIR}/admin`; do
-		ln -s "${DIR}/admin/${i}" "${HOME}/bin/${i}"
-	done
-else
-	for i in `ls ${DIR}/misc`; do
-		ln -s "${DIR}/misc/${i}" "${HOME}/bin/${i}"
-	done
-	for i in `ls ${DIR}/screen`; do
-		ln -s "${DIR}/screen/${i}" "${HOME}/bin/${i}"
-	done
-	for i in `ls ${DIR}/x11`; do
-		ln -s "${DIR}/x11/${i}" "${HOME}/bin/${i}"
-	done
-fi
+for i in `ls ${DIR}/admin`; do
+	ln -s "${DIR}/admin/${i}" "/usr/local/sbin/${i}"
+done
+for i in `ls ${DIR}/misc`; do
+	ln -s "${DIR}/misc/${i}" "/usr/local/bin/${i}"
+done
+for i in `ls ${DIR}/screen`; do
+	ln -s "${DIR}/screen/${i}" "/usr/local/bin/${i}"
+done
+for i in `ls ${DIR}/x11`; do
+	ln -s "${DIR}/x11/${i}" "/usr/local/bin/${i}"
+done
+echo "done"
