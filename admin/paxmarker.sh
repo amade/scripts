0dcf4aeea13bde8e95ee3ed54e40e904203000b5
Amadeusz Sławiński <amade@asmblr.net>
2013-10-18 00:16:24 +0200

    paxmarker.sh - search for stuff which fails to load on nvidia binaries

--- admin/paxmarker.sh
+++ admin/paxmarker.sh
@@ -1,16 +1,39 @@
 #!/bin/bash
 
+for i in `find /usr/bin -type f -executable`; do
+	rm -rf /tmp/paxit
+	ldd $i 1>/dev/null 2>/tmp/paxit
+	cat /tmp/paxit | grep libGL.so.1 >/dev/null
+	if [ $? -eq 0 ]; then
+	paxctl-ng -m "$i"
+	echo -e "\033[31mpaxmarked\033[0m ${i}"
+	fi
+done
+
+for i in `find /usr/libexec -type f -executable`; do
+	rm -rf /tmp/paxit
+	ldd $i 1>/dev/null 2>/tmp/paxit
+	cat /tmp/paxit | grep libGL.so.1 >/dev/null
+	if [ $? -eq 0 ]; then
+	paxctl-ng -m "$i"
+	echo -e "\033[31mpaxmarked\033[0m ${i}"
+	fi
+done
+
+for i in `find /usr/lib64 -type f -executable`; do
+	rm -rf /tmp/paxit
+	ldd $i 1>/dev/null 2>/tmp/paxit
+	cat /tmp/paxit | grep libGL.so.1 >/dev/null
+	if [ $? -eq 0 ]; then
+	paxctl-ng -m "$i"
+	echo -e "\033[31mpaxmarked\033[0m ${i}"
+	fi
+done
+
+
+# /usr/bin/X  needed for acceleration
 files=(	\
-	/usr/bin/blender \
-	/usr/bin/kwin \
-	/usr/bin/mplayer \
-	/usr/bin/mplayer2 \
-	/usr/bin/systemsettings \
-	/usr/bin/vlc \
-	/usr/games/bin/xonotic-glx \
-	/usr/lib64/kde4/libexec/kwin_opengl_test \
-	/usr/lib64/virtualbox/VBoxTestOGL \
-	/usr/libexec/gnome-session-check-accelerated-helper
+	/usr/bin/X
 )
 
 for i in $(seq 0 $((${#files[@]} -1))); do
--- misc/getmail.sh
+++ misc/getmail.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-while true; do
-	imapfilter &>/dev/null && offlineimap -o -u quiet &>/dev/null &
-	sleep 3600
-done
+imapfilter &>/dev/null && offlineimap -o -u quiet &>/dev/null &
